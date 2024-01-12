fff374d8d5eabe57db2db4f2b45d079b824a2533
rahul188 <rahulkuchhadia@gmail.com>
2024-01-12 16:29:10 +0530

    updated application

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+import json
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage
 import redis
-import json
 
 app = Flask(__name__)
 
